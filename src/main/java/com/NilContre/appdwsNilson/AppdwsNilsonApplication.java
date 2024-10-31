3b04c9545e1034353a5162de5629358b46d12e36
NILCONTRY <nilsondalt123456789@gmail.com>
2024-10-31 16:01:42 -0500

    Primer commit

---
+++ src/main/java/com/NilContre/appdwsNilson/AppdwsNilsonApplication.java
@@ -0,0 +1,13 @@
+package com.NilContre.appdwsNilson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdwsNilsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppdwsNilsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NilContre/appdwsNilson/AppdwsNilsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NilContre.appdwsNilson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppdwsNilsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
